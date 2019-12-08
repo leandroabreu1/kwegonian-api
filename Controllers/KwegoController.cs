1a8e6d4f0aa3d504fe305f22df1d59b2bff08937
leandroabreu1 <leandroalex1@gmail.com>
2019-12-08 18:22:39 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/KwegoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KwegonianApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KwegoController : ControllerBase
+    {
+        Kwego kwego = new Kwego();
+        
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Por favor passe como parâmetro um valor em kwegonian" ;
+        }
+
+        // GET kwego/value
+        [HttpGet("{value}")]
+        public ActionResult<string> Get(string value)
+        {
+            return kwego.KwegonianToDecimal(value);
+        }
+
+        //POST kwego/value
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] Kwego value)
+        {
+            return kwego.KwegonianToDecimal(value.Input);
+        }      
+
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Kwego.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KwegonianApi
+{
+    public class Kwego
+    {
+        public string Input { get; set; }
+
+        public string KwegonianToDecimal(string kwego)
+        {
+
+            int total = 0;
+            int ultimoValor = 0;
+            Dictionary<string, int> dicionarioValores = null;
+            try
+            {
+                if (dicionarioValores == null)
+                {
+                    dicionarioValores = new Dictionary<string, int>();
+                    dicionarioValores.Add("kil", 1);
+                    dicionarioValores.Add("jin", 5);
+                    dicionarioValores.Add("pol", 10);
+                    dicionarioValores.Add("kilow", 50);
+                    dicionarioValores.Add("jij", 100);
+                    dicionarioValores.Add("jinjin", 500);
+                    dicionarioValores.Add("polsx", 1000);
+                }
+
+                string[] kwegoArray = kwego.Split(' ');
+
+                if (kwegoArray.Length == 0) return "0";
+
+
+                for (int i = kwegoArray.Length - 1; i >= 0; i--)
+                {
+                    int valorAtual = dicionarioValores[kwegoArray[i]];
+
+                    if (valorAtual < ultimoValor)
+                        total -= valorAtual;
+                    else
+                    {
+                        total += valorAtual;
+                        ultimoValor = valorAtual;
+                    }
+                }
+
+                return total.ToString();
+            }
+            catch (KeyNotFoundException)
+            {
+                return "Algum dos valores em Kwegonian não foram encontrados";
+            }
+            catch (Exception)
+            {
+                return "Ocorreu um erro";
+            }
+        }
+    }
+}
